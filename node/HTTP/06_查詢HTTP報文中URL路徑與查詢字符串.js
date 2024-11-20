167839b9ba01e51ef4548a5dabab310a3732a00b
Tony_Lin <Tony_Lin28@pxmart.com.tw>
2024-11-20 11:41:56 +0800

    [課程51]

--- node/HTTP/06_查詢HTTP報文中URL路徑與查詢字符串.js
+++ node/HTTP/06_查詢HTTP報文中URL路徑與查詢字符串.js
@@ -5,9 +5,9 @@
   console.log('requestUrl', request.url) // /search?ke=123
     // 實例化 URL 對象
     let url = new URL(request.url, 'http://127.0.0.1:9001');
-    console.log('url', url)
+    console.log('url', url);
     // 輸出路徑
-    console.log( '路徑', url.pathname)
+    console.log( '路徑', url.pathname);
 
     // 輸出查詢字符串
     console.log( '查詢字符串', url.searchParams.get('ke'));
@@ -17,5 +17,5 @@
 
  // 監聽端口 啟動服務
  server.listen(9001, () => {
-    console.log('服務已經啟動 監聽端口9001當中...')
+    console.log('服務已經啟動 監聽端口9001當中...');
  })
---
+++ node/HTTP/07_練習.js
@@ -0,0 +1,28 @@
+// 導入HTTP模塊
+const http = require('http');
+
+// 創建服務對象
+const server = http.createServer((request, response) => {
+   response.setHeader('content-type', 'text/html;charset=utf-8');
+   
+   // 獲取請求方法
+   let {method} = request;
+   // 獲取請求URL路徑
+   let {pathname} = new URL(request.url, 'http://127.0.0.1:9001');
+   if (method === 'GET' && pathname === '/login') {
+      response.writeHead(200);
+      response.end('登入頁面');
+    } else if (method === 'GET' && pathname === '/reg') {
+      response.writeHead(200);
+      response.end('註冊頁面');
+    } else {
+      response.writeHead(404);
+      response.end('404 Not Found');
+    }
+
+})
+
+// 監聽
+server.listen(9001, () => {
+   console.log('端口9001監聽中...');
+})
