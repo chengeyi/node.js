0d38ec13537bd0e2099591e9e94027e4c8a91ad3
chenngeyi <perry564408020@gmail.com>
2024-12-04 21:38:02 +0800

    [70]

--- node/程式碼/14_模塊化暴露數計/me.js
+++ node/程式碼/14_模塊化暴露數計/me.js
@@ -11,5 +11,12 @@ function tiemo2() {
 //     tiemo2
 // };
 
-exports.tiemo = tiemo;
-exports.tiemo2 = tiemo2;
+// exports.tiemo = tiemo;
+// exports.tiemo2 = tiemo2;
+
+// 1 module.exports 可以暴露任意数据
+// module.exports = '1223';
+// module.exports = 123;
+
+// 2 不能使用 exports = value 的形式暴露数据，模块内部 module 与 exports 的隐式关系
+// exports = module.exports = {} ，require 返回的是目标模块中 module.exports 的值
